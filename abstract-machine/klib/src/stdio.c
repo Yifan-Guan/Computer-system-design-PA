2a3e16bdddbd90bdbd5f9facf31a8ce440adb0d7
tracer-ics2025 <tracer@njuics.org>
2026-04-30 03:42:10 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:42:10 up 13:15,  1 user,  load average: 0.04, 0.11, 0.09

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -68,8 +68,6 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 
     switch (*s) {
       case 'd':
-        break;
-      case 't':
         int i = va_arg(ap, int);
         a = int_to_str(i, num_buf_end);
         l = strlen(a);

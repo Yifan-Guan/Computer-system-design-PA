9185f22ea6ab1022c1e282cd27526f2c6985c265
tracer-ics2025 <tracer@njuics.org>
2026-04-30 03:25:18 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:25:18 up 12:58,  1 user,  load average: 0.22, 0.13, 0.12

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,21 +5,21 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-static char* int_to_str(int num, char* buf_end) {
-  char* p = buf_end;
-  int negative = num < 0;
-  if (negative) {
-    num = -num;
-  }
+// static char* int_to_str(int num, char* buf_end) {
+//   char* p = buf_end;
+//   int negative = num < 0;
+//   if (negative) {
+//     num = -num;
+//   }
 
-  int n = num;
-  *p-- = '\0';
-  for(; n > 10; n /= 10) *p-- = '0' + (n % 10);
-  if (n) *p = '0' + n;
-  if (negative) *--p = '-';
+//   int n = num;
+//   *p-- = '\0';
+//   for(; n > 10; n /= 10) *p-- = '0' + (n % 10);
+//   if (n) *p = '0' + n;
+//   if (negative) *--p = '-';
 
-  return p;
-}
+//   return p;
+// }
 
 int printf(const char *fmt, ...) {
   panic("Not implemented");
@@ -47,9 +47,9 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   char *s = (char*)fmt;
   int len = 0;
 
-  const int num_buf_size = 20;
-  char num_buf[num_buf_size];
-  char* num_buf_end = num_buf + num_buf_size - 1;
+  // const int num_buf_size = 20;
+  // char num_buf[num_buf_size];
+  // char* num_buf_end = num_buf + num_buf_size - 1;
 
   for(;;) {
     if(!*s || len >= n - 1) break;
@@ -67,9 +67,11 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 
     switch (*s) {
       case 'd':
-        int i = va_arg(ap, int);
-        a = int_to_str(i, num_buf_end);
-        l = num_buf_end - a;
+        // int i = va_arg(ap, int);
+        // a = int_to_str(i, num_buf_end);
+        // l = num_buf_end - a;
+        a = "123";
+        l = 3;
         break;
       case 's':
         a = va_arg(ap, char*);

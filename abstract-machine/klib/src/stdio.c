a0e1b0d561c0b7ae62113a1858735b0f96898f70
tracer-ics2025 <tracer@njuics.org>
2026-04-30 02:13:53 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:13:53 up 11:46,  1 user,  load average: 0.18, 0.18, 0.18

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -83,6 +83,7 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
       default:
         assert(0);
     }
+  assert(0);
 
     len += l;
     if (len >= n) {
@@ -90,7 +91,6 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
       out[n - 1] = '\0';
     }
     strncpy(out, a, l);
-  assert(0);
   }
 
   return len;

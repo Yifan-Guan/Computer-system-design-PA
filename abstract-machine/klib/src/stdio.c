723ad5d2228707526219059257c86a76c20ec045
tracer-ics2025 <tracer@njuics.org>
2026-04-30 01:58:45 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:58:45 up 11:31,  1 user,  load average: 0.11, 0.17, 0.17

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,37 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static char* int_to_str(int num, char* buf_end) {
+  char* p = buf_end;
+  int negative = num < 0;
+  if (negative) {
+    num = -num;
+  }
+
+  int n = num;
+  *p-- = '\0';
+  for(; n > 10; n /= 10) *p-- = '0' + (n % 10);
+  if (n) *p = '0' + n;
+  if (negative) *--p = '-';
+
+  return p;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  return vsnprintf(out, (size_t)-1, fmt, ap);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  int ret;
+  va_list ap;
+  va_start(ap, fmt);
+  ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -22,7 +43,56 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *a, *z, *s = (char*)fmt;
+  int l = 0, len = 0;
+
+  const int num_buf_size = 14;
+  char num_buf[num_buf_size];
+  char* num_buf_end = num_buf + num_buf_size - 1;
+
+  for(;;) {
+    if(!*s) break;
+
+    for(a=s; *s && *s != '%'; s++);
+
+    for(z=s; s[0] == '%' && s[1] == '%'; z++, s+=2);
+    l = z - a;
+    assert(l >= 0);
+    len += l;
+    if (len >= n) {
+      l = n - len - 1;
+      out[n - 1] = '\0';
+    }
+    strncpy(out, a, l);
+    continue;
+
+    s++;
+
+    switch (*s) {
+      case 'd':
+        int i = va_arg(ap, int);
+        a = int_to_str(i, num_buf_end);
+        z = num_buf_end;
+        l = z - a;
+        break;
+      case 's':
+        a = va_arg(ap, char*);
+        l = strlen(a);
+        if (a == NULL) a = "(null)";
+        break;
+      default:
+        assert(0);
+    }
+
+    len += l;
+    if (len >= n) {
+      l = n - len - 1;
+      out[n - 1] = '\0';
+    }
+    strncpy(out, a, l);
+  }
+
+  return 1;
 }
 
 #endif

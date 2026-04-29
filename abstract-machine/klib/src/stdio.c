708a83ad5ef211594e13c5a3d6bde0b4878c6361
tracer-ics2025 <tracer@njuics.org>
2026-04-30 02:12:06 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:12:06 up 11:44,  1 user,  load average: 0.32, 0.21, 0.19

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -43,6 +43,7 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  assert(0);
   char *a, *z;
   char *s = (char*)fmt;
   int l = 0, len = 0;

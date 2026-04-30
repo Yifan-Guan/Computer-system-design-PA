bdefb1bc9ae52cfa218b20936ba1124bcba94bec
tracer-ics2025 <tracer@njuics.org>
2026-04-30 08:35:06 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:35:06 up 18:08,  1 user,  load average: 0.14, 0.10, 0.09

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,8 @@
 
 void __am_gpu_init() {
    int i;
-   int w = 400 * 32;  // TODO: get the correct width
-   int h = 300 * 32;  // TODO: get the correct height
+   int w = 400;  // TODO: get the correct width
+   int h = 300;  // TODO: get the correct height
    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
    for (i = 0; i < w * h; i ++) fb[i] = i;
    outl(SYNC_ADDR, 1);
@@ -15,7 +15,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 400 * 32, .height = 300 * 32,
+    .width = 400, .height = 300,
     .vmemsz = 0
   };
 }

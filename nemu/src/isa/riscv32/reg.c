7fb9e5abcf0089c5fd7de23e919a7630fe41ceda
tracer-ics2025 <tracer@njuics.org>
2026-05-26 11:01:10 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:01:10 up  1:06,  1 user,  load average: 0.17, 0.14, 0.10

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -23,6 +23,10 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
+const char *csr_regs[] = {
+  "mtvec", "mepc", "mstatus", "mcause"
+};
+
 static int reg_num = 32;
 
 void isa_reg_display() {
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,13 +14,17 @@
 ***************************************************************************************/
 
 #include <isa.h>
+#include "../local-include/reg.h"
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
 
-  return 0;
+  csr(SR_MCAUSE) = NO;
+  csr(SR_MEPC) = epc;
+
+  return csr(SR_MTVEC);
 }
 
 word_t isa_query_intr() {

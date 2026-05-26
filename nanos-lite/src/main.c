9e7bce6c49ecabc5f15856066a274eaedf2f33ea
tracer-ics2025 <tracer@njuics.org>
2026-05-26 20:34:36 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:34:36 up  1:17,  1 user,  load average: 0.20, 0.17, 0.12

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -10,8 +10,10 @@ void init_proc(void);
 int main() {
   extern const char logo[];
   printf("%s", logo);
+  printf("label1\n");
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
+  printf("label2\n");
 
   init_mm();
 

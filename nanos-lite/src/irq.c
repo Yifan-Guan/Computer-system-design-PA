630c603dca12127b31b63e692126f8483c983e80
tracer-ics2025 <tracer@njuics.org>
2026-05-27 22:36:20 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:36:20 up  1:10,  1 user,  load average: 0.17, 0.16, 0.13

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,9 +1,11 @@
 #include <common.h>
 
+void do_syscall(Context *c);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("EVENT_YIELD triggered @ %p\n", c->mepc); break;
-    case EVENT_SYSCALL: printf("EVENT_SYSCALL triggered @ %p\n", c->mepc); break;
+    case EVENT_SYSCALL: do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 

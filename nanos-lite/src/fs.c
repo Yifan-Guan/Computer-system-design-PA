33b8a0159c41c7d9834a091a8b4b997c95cee359
tracer-ics2025 <tracer@njuics.org>
2026-05-29 11:35:48 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:35:48 up  3:10,  1 user,  load average: 0.50, 0.22, 0.13

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,7 +1,10 @@
+#include <common.h>
 #include <fs.h>
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
+
+size_t serial_write(const void *buf, size_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -31,7 +34,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
@@ -57,46 +60,34 @@ int fs_open(const char *pathname, int flags, int mode) {
 
 size_t fs_read(int fd, void *buf, size_t len) {
   assert(fd >= 0 && fd < sizeof(file_table) / sizeof(Finfo));
-  switch (fd) {
-    case FD_STDIN: return 0;
-    case FD_STDOUT: case FD_STDERR: return 0;
-    default: 
-      size_t f_size = file_table[fd].size;
-      size_t f_open_offset = file_table[fd].open_offset;
-      size_t f_disk_offset = file_table[fd].disk_offset;
-      size_t f_remain = f_size - f_open_offset;
-      if (len > f_remain) {
-        len = f_remain;
-      }
-      size_t ret = file_table[fd].read(buf, f_disk_offset + f_open_offset, len);
-      file_table[fd].open_offset += ret;
-      return ret;
+
+  size_t f_size = file_table[fd].size;
+  size_t f_open_offset = file_table[fd].open_offset;
+  size_t f_disk_offset = file_table[fd].disk_offset;
+  size_t f_remain = f_size - f_open_offset;
+  if (len > f_remain) {
+    len = f_remain;
   }
-  
+  size_t ret = file_table[fd].read(buf, f_disk_offset + f_open_offset, len);
+  file_table[fd].open_offset += ret;
+
+  return ret;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   assert(fd >= 0 && fd < sizeof(file_table) / sizeof(Finfo));
-  switch (fd) {
-    case FD_STDIN: return 0;
-    case FD_STDOUT: case FD_STDERR: 
-      for (size_t i = 0; i < len; i++) {
-        putch(((char *)buf)[i]);
-      }
-      return len;
-    default: 
-      size_t f_size = file_table[fd].size;
-      size_t f_open_offset = file_table[fd].open_offset;
-      size_t f_disk_offset = file_table[fd].disk_offset;
-      size_t f_remain = f_size - f_open_offset;
 
-      if (len > f_remain) {
-        len = f_remain;
-      }
-      size_t ret = file_table[fd].write(buf, f_disk_offset + f_open_offset, len);
-      file_table[fd].open_offset += ret;
-      return ret;
+  size_t f_size = file_table[fd].size;
+  size_t f_open_offset = file_table[fd].open_offset;
+  size_t f_disk_offset = file_table[fd].disk_offset;
+  size_t f_remain = f_size - f_open_offset;
+  if (len > f_remain) {
+    len = f_remain;
   }
+  size_t ret = file_table[fd].write(buf, f_disk_offset + f_open_offset, len);
+  file_table[fd].open_offset += ret;
+
+  return ret;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -2,17 +2,12 @@
 #include <assert.h>
 
 int main() {
-  printf("test begin");
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
-
-  printf("open");
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
-
-  printf("size");
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
@@ -21,21 +16,15 @@ int main() {
     assert(n == i + 1);
   }
 
-  printf("read");
-
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  printf("write");
-
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
-  printf("verify");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
@@ -43,11 +32,7 @@ int main() {
     assert(n == i + 1 + 1000);
   }
 
-  printf("verify again");
-
   fclose(fp);
-
-  printf("close");
 
   printf("PASS!!!\n");
 

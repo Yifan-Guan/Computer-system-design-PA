4023f6626b00622f4de15595f887cab32577a9a4
tracer-ics2025 <tracer@njuics.org>
2026-05-29 09:59:21 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  09:59:21 up  1:34,  1 user,  load average: 0.44, 0.24, 0.16

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -5,9 +5,13 @@ int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
+  printf("open");
+
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
+
+  printf("size");
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
@@ -16,15 +20,21 @@ int main() {
     assert(n == i + 1);
   }
 
+  printf("read");
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
+  printf("write");
+
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+
+  printf("verify");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
@@ -32,7 +42,11 @@ int main() {
     assert(n == i + 1 + 1000);
   }
 
+  printf("verify again");
+
   fclose(fp);
+
+  printf("close");
 
   printf("PASS!!!\n");
 

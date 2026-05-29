c7a30c783cb03e36244cedef48d9a1e8958f03dc
tracer-ics2025 <tracer@njuics.org>
2026-05-29 18:40:32 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:40:32 up  4:54,  1 user,  load average: 0.11, 0.20, 0.18

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -20,6 +20,8 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
 
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
+
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -37,7 +39,7 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, serial_write},
   [FD_EVENT]  = {"/dev/events", 0, 0, 0, events_read, invalid_write},
-  [FD_PBINFO] = {"/proc/dispinfo", 0, 0, 0, invalid_read, invalid_write},
+  [FD_PBINFO] = {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
   [FD_PB]     = {"/dev/fb", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };

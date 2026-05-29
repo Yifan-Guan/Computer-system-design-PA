c12204d8b9d9ffd121de1d34bfafda0e2b9562a3
tracer-ics2025 <tracer@njuics.org>
2026-05-29 13:49:22 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:49:22 up 5 min,  1 user,  load average: 0.08, 0.16, 0.09

--- navy-apps/tests/time-test/time-test.c
+++ navy-apps/tests/time-test/time-test.c
@@ -12,6 +12,7 @@ int main() {
     gettimeofday(&current, NULL);
     if (current.tv_sec * 1000 + current.tv_usec - start.tv_sec * 1000 - start.tv_usec >= 500) {
       printf("Current time: %d.%d seconds\n", current.tv_sec, current.tv_usec);
+      start = current;
     }
   }
   return 0;

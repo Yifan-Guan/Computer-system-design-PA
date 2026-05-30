26365bd33ca07d6e6c1e2c0cb73b6c4acee1e52f
tracer-ics2025 <tracer@njuics.org>
2026-05-30 18:50:26 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:50:26 up  5:57,  1 user,  load average: 0.22, 0.21, 0.21

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -36,6 +36,9 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
   paddr_t pte_addr = satp_ppn * 4096 + (vpn1>>22) * 4;
 
   word_t pte = host_read(guest_to_host(pte_addr), 4);
+  if(!(pte&1)) {
+    panic("invalid PTE: vaddr=0x%x, pte_addr=0x%x, pte=0x%x\n", vaddr, pte_addr, pte);
+  } 
 
   if ((pte&0x2)==0 && (pte&0x4)==0 && (pte&0x8)==0) {
     

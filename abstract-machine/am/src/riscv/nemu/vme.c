4160733903327d79aa091957fbdff20c792b6cb1
tracer-ics2025 <tracer@njuics.org>
2026-05-30 19:01:37 +0800

    >  compile NEMU 2312307 管一凡 Linux LAPTOP-LG4U2OPA 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:01:37 up  6:08,  1 user,  load average: 0.08, 0.11, 0.17

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -74,7 +74,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   uintptr_t vpn1, vpn0;
   uintptr_t ppn1, ppn0;
   uintptr_t pte;
-  uintptr_t pte1_addr, pte0_addr;
+  uintptr_t pte1_addr, pte0_base, pte0_addr;
   uintptr_t v=1, r=1<<1, w=1<<2, x=1<<3;
 
   vpn1 = (uintptr_t) va & 0xffc00000;
@@ -85,8 +85,10 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   pte1_addr = pdir * PGSIZE + (vpn1>>22) * PTESIZE;
 
   if (*(uintptr_t*)pte1_addr == 0) {
-    pte0_addr = (uintptr_t) pgalloc_usr(PGSIZE);
-    *(uintptr_t*)pte1_addr = ((pte0_addr & 0xfffff000) >>2) | v;
+    pte0_base = (uintptr_t) pgalloc_usr(PGSIZE);
+    *(uintptr_t*)pte1_addr = ((pte0_base & 0xfffff000) >>2) | v;
+
+    pte0_addr = pte0_base + (vpn0 >> 12) * PTESIZE;
 
   } else {
     uintptr_t pte_ppn = ((*(uintptr_t*)pte1_addr) & 0xfffffc00) >> 10;
